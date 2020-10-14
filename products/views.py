a4f3e085e00514ed9d416a4ac127419a863af44f
jsandragadomska <nimuehh07@hmail.com>
2020-10-14 20:02:34 +0200

    basic views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse, JsonResponse, Http404
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello world</h1>")
+
+def product_detail_view(request, id):
+    try:
+        obj = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        raise Http404
+
+    return HttpResponse(f"Product name: {obj.title}")
+
+def product_api_detail_view(request, id, *args, **kwargs):
+    try:
+        obj = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        return JsonResponse({"message": "Not found"})
+
+    return JsonResponse({"id": obj.id})
