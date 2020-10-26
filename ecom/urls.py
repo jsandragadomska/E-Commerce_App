1f5d39ff44ddc147ebfa8d9fd3da856799c0e7ed
jsandragadomska <nimuehh07@hmail.com>
2020-10-26 18:52:48 +0100

    Cleanup

--- ecom/urls.py
+++ ecom/urls.py
@@ -1,31 +1,18 @@
-"""ecom URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, re_path
 from products.views import (
-    home_view,
+    search_view,
     product_detail_view,
     product_api_detail_view,
-    product_list_view
+    product_list_view,
+    product_create_view,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('search/', home_view),
+    path('search/', search_view),
     path('products/', product_list_view),
     path('products/<int:pk>/', product_detail_view),
-    re_path(r'api/products/(?P<pk>\d+)/', product_api_detail_view),   
+    re_path(r'api/products/(?P<pk>\d+)/', product_api_detail_view), 
+    path('products/create/', product_create_view),  
 ]
