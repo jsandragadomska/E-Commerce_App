37429cd39581995474db003634d93fd09fc25d4e
jsandragadomska <nimuehh07@hmail.com>
2020-11-08 16:22:05 +0100

    authentication of user

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
 from .forms import LoginForm
@@ -9,7 +9,9 @@ def login_view(request):
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
         user = authenticate(request, username="username", password="password")
-        if user == None:
-#            attempt = request.session.get("attempt") or 0
-#            request.session['attempt'] = attempt + 1
-            return redirect("/invalid-password")
+        if user != None:
+            login(request, user)
+            return redirect("/search")
+        else:
+            request.session['invalid_user'] = 1
+    return render(request, "forms.html", {"form": form})
