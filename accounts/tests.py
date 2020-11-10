12fe65ab6546f5915887cb693571ed8c72f96337
jsandragadomska <nimuehh07@hmail.com>
2020-11-10 13:44:35 +0100

    Test_user_password

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 User = get_user_model()
 # Create your tests here.
-class UserTestCast(TestCase):
+class UserTestCase(TestCase):
     def setUp(self):
         user_a = User(username='cfe', email='email@email.com')
         user_a_pw = 'cfepassword'
@@ -21,3 +22,13 @@ class UserTestCast(TestCase):
 
     def test_user_password(self):
         self.assertTrue(self.user_a.check_password(self.user_a_pw))
+
+    def test_login_url(self):
+        login_url = settings.LOGIN_URL
+        data = {'username': 'cfe', 'password': self.user_a_pw}
+        response = self.client.post(login_url, data, follow=True)
+        status_code = response.status_code
+        redirect_path = response.request.get("PATH_INFO")
+        self.assertEqual(redirect_path, settings.LOGIN_REDIRECT_URL)
+        self.assertEqual(status_code, 200)
+
--- accounts/views.py
+++ accounts/views.py
@@ -13,14 +13,14 @@ def login_view(request):
         user = authenticate(request, username="username", password="password")
         if user != None:
             login(request, user)
-            return redirect("/home")
+            return redirect("/")
         else:
             request.session['invalid_user'] = 1
     return render(request, "accounts/forms.html", {"form": form})
 
 def logout_view(request):
     logout(request)
-    return redirect("/home")
+    return redirect("/")
 
 def register_view(request):
     form = RegisterForm(request.POST or None)
@@ -35,7 +35,7 @@ def register_view(request):
             user = None
         if user != None:
             login(request, user)
-            return redirect("/home")
+            return redirect("/")
         else:
             request.session['register_error'] = 1
     return render(request, "accounts/forms.html", {"form": form})
