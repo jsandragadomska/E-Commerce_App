50c83b52f865fbd45d2ea5e5d02659174ba2db48
jsandragadomska <nimuehh07@hmail.com>
2020-11-23 13:00:49 +0100

    product fearured added yo the model

--- products/models.py
+++ products/models.py
@@ -16,7 +16,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
     amount = models.IntegerField(null=True, blank=True)
     category = models.CharField(max_length=225, default='None')
-    product_pic = models.ImageField(null=True, blank=True, upload_to="images/products/")
+    featured = models.BooleanField(default=False)
+    product_pic = models.ImageField(null=True, blank=True, upload_to="ecom/images/products/")
 
     def has_amount(self):
         return self.amount > 0
