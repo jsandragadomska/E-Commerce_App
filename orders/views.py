3bd4da676ef0e78154aa853baa5641d8a9029ff6
jsandragadomska <nimuehh07@hmail.com>
2020-11-24 15:51:41 +0100

    order form and view upgrade

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 
 from products.models import Product
 from .models import Order
+from .forms import OrderForm
 
 @login_required
 def order_checkout_view(request):
@@ -13,11 +14,20 @@ def order_checkout_view(request):
     user = request.user
     order_id =request.session.get("order_id")
     order_obj = None
+    new_creation = False
     try:
         order_obj = Order.objects.get(id=order_id)
     except:
         order_id = None
     if order_id == None:
+        new_creation = True
         order_obj = Order.objects.create(product=product, user=user)
-        request.session['order_id'] = order_obj.id
-    return render(request, 'products/forms.html', {})
+    if order_obj != None and new_creation == False:
+        if order_obj.product.id != product.id:
+            order_obj = Order.objects.create(product=product, user=user)
+    request.session['order_id'] = order_obj.id
+    form = OrderForm(request.POST or None, product=product)
+    if form.is_valid():
+        print(form.cleaned_data.get("shipping_address"))
+        print(form.cleaned_data.get("billing_address"))
+    return render(request, 'products/forms.html', {"form": form})
